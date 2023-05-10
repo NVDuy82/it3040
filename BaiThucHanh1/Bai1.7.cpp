724bfb86a991605cc0e724b91331fe5b8d986da6
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-05-10 08:41:35 +0700

    Create Bai1.7.cpp

---
+++ BaiThucHanh1/Bai1.7.cpp
@@ -0,0 +1,57 @@
+// Nguyen Van Duy - 20215334
+/*
+Bài 1.7. Viết chương trình nhập vào một mảng các
+số nguyên với số lượng các phần tử nhập từ bàn phím.
+Sau đó sắp xếp mảng theo thứ tự tăng dần.
+Hiển thị danh sách mảng trước và sau khi sắp xếp.
+*/
+#include <stdio.h>
+#include <cstdlib>
+int *a;
+int n, tmp;
+
+int main(){
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    //#Allocate memory
+    
+    /*****************
+    # YOUR CODE HERE #
+    *****************/
+    a = (int *) malloc(n * sizeof(int));
+    /****************/
+    
+    for(int i = 0; i < n; i++)
+        scanf("%d", a + i); 
+    
+    printf("The input array is: \n");
+    for(int i = 0; i < n; i++)
+        printf("%d ", *(a + i));
+    printf("\n");
+    
+    //#Sort array
+    
+    /*****************
+    # YOUR CODE HERE #
+    *****************/
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = 0; j < n-i-1; ++j) {
+            if (*(a+j) > *(a+j+1)) {
+                // swap
+                int tmp = *(a+j);
+                *(a+j) = *(a+j+1);
+                *(a+j+1) = tmp;
+            }
+        }
+    }
+    
+    printf("The sorted array is: \n");
+    for(int i = 0; i < n; i++)
+        printf("%d ", *(a + i));
+    printf("\n");
+    
+    delete [] a;
+    return 0;
+}
+// Nguyen Van Duy - 20215334
