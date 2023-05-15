a70f251b1179c8ad0151fe280e884ac5e0d27413
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-05-15 23:09:50 +0700

    Update and rename Bai1.5.cpp to Bai1.5.c

--- BaiThucHanh1/Bai1.5.c
+++ BaiThucHanh1/Bai1.5.c
@@ -4,11 +4,13 @@ Bài 1.5. Viết hàm trả về con trỏ trỏ tới
 giá trị lớn nhất của một mảng các số double.
 Nếu mảng rỗng hãy trả về NULL.
 */
+#include <stdio.h>
+
 double* maximum(double* a, int size){
     double *max;
     max = a;
     if (a==NULL) return NULL;
-
+    
     /*****************
     # YOUR CODE HERE #
     *****************/
@@ -19,5 +21,12 @@ double* maximum(double* a, int size){
     }
     
     return max;
+}
+
+int main() {
+    double arr[] = {1., 10., 2., -7., 25., 3.};
+    double* max = maximum(arr, 6);
+    printf("%.0f", *max);
+    return 0;
 }
 // Nguyen Van Duy - 20215334
