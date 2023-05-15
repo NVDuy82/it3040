b6c1716aec4912f5aad529aec351de3b3f1c9804
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-05-15 23:12:22 +0700

    Update Bai1.6.cpp

--- BaiThucHanh1/Bai1.6.cpp
+++ BaiThucHanh1/Bai1.6.cpp
@@ -4,6 +4,10 @@ Bài 1.6. Viết hàm đảo ngược một mảng
 các số nguyên theo hai cách:
 dùng chỉ số và dùng con trỏ.
 */
+#include <iostream>
+
+using namespace std;
+
 void reversearray(int arr[], int size){
     int l = 0, r = size - 1, tmp;
     
@@ -36,4 +40,13 @@ void ptr_reversearray(int *arr, int size){
     }
 }
 
+int main() {
+    int arr[] = {9, 3, 5, 6, 2, 5};
+    reversearray(arr, 6);
+    for(int i = 0; i < 6; i++) cout << arr[i] << " ";
+    int arr2[] = {4, -1, 5, 9};
+    ptr_reversearray(arr2, 4);
+    for(int i = 0; i < 4; i++) cout << arr2[i] << " ";
+    return 0;
+}
 // Nguyen Van Duy - 20215334
