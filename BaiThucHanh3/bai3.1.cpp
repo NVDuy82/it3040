3d9b573ff988480ccbf0feabd0eddc2c61e7b164
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-06-14 21:30:24 +0700

    Update bai3.1.cpp

--- BaiThucHanh3/bai3.1.cpp
+++ BaiThucHanh3/bai3.1.cpp
@@ -12,9 +12,10 @@ int lucas(int n) {
     /*****************
     # YOUR CODE HERE #
     *****************/
-    if (n == 0) return 1;
-    if (n == 1) return 2;
+    if (n == 0) return 2;  // lucas(0) = 2
+    if (n == 1) return 1;  // lucas(1) = 1
     
+    // tinh lucas bang de quy
     return lucas(n-1) + lucas(n-2);
 }
 
