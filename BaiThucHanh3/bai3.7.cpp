e03e75d0c98e3c2db7492cbe5e38857b2819db65
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-06-14 23:06:39 +0700

    Update bai3.7.cpp

--- BaiThucHanh3/bai3.7.cpp
+++ BaiThucHanh3/bai3.7.cpp
@@ -30,27 +30,34 @@ int main() {
             s.pop();
             continue;
         }
-
+        
         //# Khử đệ quy
         /*****************
         # YOUR CODE HERE #
         *****************/
+        
+        // luu cac gia tri o ngan dau tien stack
         int i = top.i, j = top.j, old_L = top.old_L;
         s.pop();
-
-        x[i] = j;
-
+        
+        x[i] = j; // ghi gia tri j vao vi tri thu i
+        
         if (j) {
+            // neu j = 1 thi so bit 1 o cuoi day tang them 1
             L = old_L + 1;
         } else {
+            // neu j = 0 thi khong co bit 1 nao o cuoi day
             L = 0;
             if (old_L + 1 < k) {
+                // dua vao stack 1 state (ghi gia tri 1 vao vi tri thu i)
+                // thay the so 0 o vi tri hien tai thanh so 1
                 s.push(state(i, 1, old_L));
             }
         }
-
+        
+        // dua vao stack 1 state (ghi gia tri 0 vao vi tri thu i+1)
+        // vi tri tiep theo se nhan gia tri 0
         s.push(state(i + 1, 0, L));
-
     }
     return 0;
 }
