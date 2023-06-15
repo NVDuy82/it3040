4fb9cbaf752b3221af9abbca8be22e218478df96
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-06-15 22:27:01 +0700

    Update bai4.2.cpp

--- BaiThucHanh4/bai4.2.cpp
+++ BaiThucHanh4/bai4.2.cpp
@@ -1,3 +1,9 @@
+// Nguyen Van Duy - 20215334
+/*
+Bài 4.2. Một điểm trong không gian 2 chiều được biểu diễn bằng pair.
+Hãy viết hàm double area(Point a, Point b, Point c) tính diện tích tam giác theo tọa độ 3 đỉnh.
+Trong đó, Point là kiểu được định nghĩa sẵn trong trình chấm như sau: using Point = pair<double, double>;
+*/
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -19,3 +25,4 @@ int main() {
     cout << area({1, 2}, {2.5, 10}, {15, -5.25}) << endl;
     return 0;
 }
+// Nguyen Van Duy - 20215334
