e20b68945e8715f9fdca1c975c6244fa504a505b
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-06-28 19:55:48 +0700

    Update bai4.13.cpp

--- BaiThucHanh4/bai4.13.cpp
+++ BaiThucHanh4/bai4.13.cpp
@@ -5,30 +5,43 @@ Hãy viết chương trình đếm số lượng xâu con chứa số ký tự 0
 */
 
 #include <iostream>
-#include <vector>
+#include <map>
 
 using namespace std;
 
-inline unsigned long long int count(string &s) {
-    unsigned long long int count = 0; // result
-    int n = s.length(); // get length of string
-    vector<int> sum(n, 0); // sum[i] : sum from 0 to i
-    for (int i = 0; i < s.length(); i++) { // loop through string
-        for (int j = 0; j <= i; ++j) { // loop from 0 to i
-            sum[j] += s[i] == '1' ? 1 : -1; // +1 if binary 1, -1 if binary 0
-            if (sum[j] == 0) { // check if number of 0 == number of 1
-                count++; // increment counter
-            }
-        }
-    }
-    return count; // return
+inline int calc(int v) {
+    // voi so lan xuat hien chenh lech la v, thi so xau dem duoc la:
+    // result = 1 + 2 + 3 + ... + (v-1) = v * (v-1) / 2
+    return v * (v-1) / 2;
 }
 
 int main() {
-    string s; // binary string
-    cin >> s; // input binary string
-    cout << count(s) << endl; // count substring
+    string str;
+    cin >> str;
     
+    map<int, int> m; // <chenh lech : so lan>
+    map<int, int>::iterator it;
+    int chenh_lech = 0; // chenh lech = so luong 1 - so luong 0
+    
+    for (char c : str) {
+        if (c == '0') chenh_lech--; // giam 1 neu xuat hien 0
+        else chenh_lech++; // tang 1 neu xuat hien 1
+        
+        it = m.find(chenh_lech);
+        if (it != m.end()) { // neu do chenh lech da ton tai
+            it->second += 1; // tang so lan xuat hien cua chenh lech
+        } else {
+            m.insert({chenh_lech, 1});
+        }
+    }
+    
+    int res = 0; // result
+    for (it = m.begin(); it != m.end() ; ++it) {
+        res += calc(it->second);
+        if (it->first == 0) res += it->second; // neu chenh lech = 0
+    }
+    
+    cout << res; // print result
     return 0;
 }
 
