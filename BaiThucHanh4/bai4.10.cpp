6f7cc69b5a553dfaf8e95fd77de9e5ffce84644c
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-06-28 18:20:59 +0700

    Update bai4.10.cpp

--- BaiThucHanh4/bai4.10.cpp
+++ BaiThucHanh4/bai4.10.cpp
@@ -14,332 +14,112 @@ Sử dụng phương pháp tính điểm TF-IDF
 
 using namespace std;
 
-class Document {
-private:
-    vector<string> t;
-    map<string, int> f;
-    map<string, double> tf;
-public:
-    Document() = default;
+int n, q;
+vector<vector<string>> text_vector; // n texts
+vector<vector<string>> query_vector; // q queries
+map<pair<string, int>, int> f; // frequency of occurrence of the word @t in the text[i] (pair<string, int>)
+vector<int> max_f; // maximum frequency of occurrence in the text[i]
+map<string, int> df; // number of text containing the word t
 
-    explicit Document(const vector<string> &t) : t(t) {}
-
-    ~Document() {
-        t.clear();
-        t.shrink_to_fit();
-        f.clear();
-        tf.clear();
-    }
-
-    [[nodiscard]] int size() const { return t.size(); }
-
-    [[nodiscard]] const string &operator[](int i) const { return t[i]; }
-
-    Document & operator=(const Document & d) = default;
-
-    friend ostream &operator<<(ostream &os, const Document d) {
-        int i;
-        for (i = 0; i < d.size() - 1; ++i) {
-            os << d[i] << ", ";
-        }
-        os << d[i];
-        return os;
-    }
-
-    friend istream &operator>>(istream &is, Document &d) {
-        string s;
-        is >> s;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ',') {
-                d.t.push_back(s.substr(0, i));
-                s = s.substr(i + 1);
-                i = -1;
-            }
-        }
-        d.t.push_back(s);
-        return is;
-    }
-
-    friend bool operator==(const Document &d1, const Document &d2) {
-        return d1.t == d2.t;
-    }
-
-    friend bool operator!=(const Document &d1, const Document &d2) {
-        return !(d1 == d2);
-    }
-
-    friend bool operator<(const Document &d1, const Document &d2) {
-        return d1.t < d2.t;
-    }
-
-    friend bool operator>(const Document &d1, const Document &d2) {
-        return d1.t > d2.t;
-    }
-
-    friend bool operator<=(const Document &d1, const Document &d2) {
-        return d1.t <= d2.t;
-    }
-
-    friend bool operator>=(const Document &d1, const Document &d2) {
-        return d1.t >= d2.t;
-    }
-
-    friend Document operator+(const Document &d1, const Document &d2) {
-        Document d(d1.t);
-        d.t.insert(d.t.end(), d2.t.begin(), d2.t.end());
-        return d;
-    }
-
-    int f_td(string _t, bool recalc = false) {
-        if (f.find(_t) != f.end() && !recalc) {
-            return f[_t];
+// split string by ','
+inline vector<string> split_string(const string &s) {
+    vector<string> res; // result
+    string t;
+    for (char i: s) {
+        if (i != ',') {
+            t.push_back(i); // add to t or t += i
         } else {
-            int count = 0;
-            for (const auto & i : t) {
-                if (i == _t) {
-                    count++;
-                }
+            res.push_back(t); // add t to vector res
+            t.clear(); // t = ""
+        }
+    }
+    res.push_back(t); //
+    return res; // return result
+}
+
+inline void input() {
+    cin >> n; // input n
+    for (int i = 0; i < n; ++i) {
+        string temp; // allocate
+        cin >> temp; // get string
+        text_vector.push_back(split_string(temp)); // add split string
+    }
+    cin >> q; // input q
+    for (int i = 0; i < q; ++i) {
+        string temp; // allocate
+        cin >> temp; // get string
+        query_vector.push_back(split_string(temp)); // add split string
+    }
+}
+
+inline void calc_f() {
+    for (int i = 0; i < n; ++i) {
+        int maximum = 0; // max f
+        for (auto &t : text_vector[i]) {
+            pair<string, int> key = make_pair(t, i); // key : word @t in text[i]
+            if (f.find(key) != f.end()) { // check if key exists
+                ++f[key]; // increase f
+            } else {
+                f[key] = 1; // set f
             }
-            f[_t] = count;
-            return count;
+            maximum = max(maximum, f[key]); // check and update maximum
         }
+        max_f.push_back(maximum); // push_back max f to vector
     }
+}
 
-    double tf_td(string _t, bool recalc = false) {
-        if (tf.find(_t) != tf.end() && !recalc) {
-            return tf[_t];
-        } else {
-            double res; // result
-            int max_f = 0;
-            for (const auto & i : f) {
-                if (i.second > max_f) {
-                    max_f = i.second;
-                }
-            }
-            res = max_f ? (0.5 + 0.5 * f_td(_t) / max_f) : 0;
-            tf[_t] = res;
-            return res;
-        }
-    }
-
-    bool contains(string _t) {
-        return f_td(_t, true) != 0;
-    }
-};
-
-class Documents {
-private:
-    vector<Document> d;
-public:
-    Documents() = default;
-
-    explicit Documents(const vector<Document> &d) {
-        this->d = d;
-    }
-
-    explicit Documents(int n) {
-        for (int i = 0; i < n; ++i) {
-            d.emplace_back();
-        }
+inline int calc_df(const string& t) {
+    if (df.find(t) != df.end()) { // check if t exists
+        return df[t]; // return calculated result
     }
     
-    ~Documents() {
-        d.clear();
-        d.shrink_to_fit();
-    }
-
-    void add(const Document& doc) {
-        this->d.push_back(doc);
-    }
-
-    void remove(const int index) {
-        d.erase(d.begin() + index);
-    }
-
-    void remove(const Document& doc) {
-        for (int i = 0; i < d.size(); ++i) {
-            if (d[i] == doc) {
-                d.erase(d.begin() + i);
-                return;
+    int res = 0; // result
+    for (int i = 0; i < n; ++i) {
+        for (string &s : text_vector[i]) {
+            if (t == s) {
+                ++res; // increase 1
+                break;
             }
         }
     }
+    df[t] = res; // update result
+    return res; // return result
+}
 
-    [[nodiscard]] const Document &operator[](int i) const { return d[i]; }
-
-    Documents &operator=(const Documents& docs) {
-        this->d.clear();
-        for (int i = 0; i < docs.size(); i++) {
-            this->d.push_back(docs[i]);
-        }
-        return *this;
-    }
-
-    [[nodiscard]] int size() const { return d.size(); }
-
-    friend ostream & operator<<(ostream & os, Document &documents) {
-        for (int i = 0; i < documents.size(); ++i) {
-            os << documents[i] << endl;
-        }
-        return os;
-    }
-
-    int df(string t) {
-        int count = 0;
-        for (auto & i : d) {
-            if (i.contains(t)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    double idf(string t) {
-        int n = d.size();
-        return df(t) ? log2(n / df(t)) : 0;
-    }
-
-    double score(string t, int index_doc) {
-        return d[index_doc].tf_td(t) * idf(t);
-    }
-};
-
-class Query {
-private:
-    vector<string> t;
-    vector<double> score;
-public:
-    Query() = default;
-
-    explicit Query(const vector<string> &t) : t(t) {}
+inline double score(const string& t, int index_text) {
+    if (f[{t, index_text}] == 0) return 0; // score = 0
     
-    ~Query() {
-        t.clear();
-        t.shrink_to_fit();
-        score.clear();
-        score.shrink_to_fit();
-    }
+    // score = tf * idf
+    // trong do :
+    //   tf = 0.5 + 0.5 * f / max_f
+    //   idf = log2 (N / df)
+    return (0.5 + 0.5 * f[{t, index_text}] / max_f[index_text]) * log2(n / calc_df(t));
+}
 
-    void calc(Documents documents) {
-        for (int i = 0; i < documents.size(); ++i) {
-            score.push_back(0.0);
-            for (int j = 0; j < t.size(); ++j) {
-                score[i] += documents.score(t[j], i);
-            }
+inline int search_engine(vector<string> &queries) {
+    int res = -1; // result
+    double max_score = -1; // maximum score
+    for (int i = 0; i < n; ++i) {
+        double score_cur = 0; // current score
+        for (auto &t : queries) {
+            score_cur += score(t, i); // calculate and increase
+        }
+        
+        if (score_cur > max_score) { // check if max score
+            max_score = score_cur; // update max score
+            res = i; // update index (result)
         }
     }
-
-    double get_score(int index_doc) {
-        return score[index_doc];
-    }
-
-    int search_index() {
-        int res = 0;
-        for (int i = 0; i < score.size(); ++i) {
-            if (score[i] > score[res]) {
-                res = i;
-            }
-        }
-        return ++res;
-    }
-
-    [[nodiscard]] int size() { return t.size(); }
-
-    [[nodiscard]] const string &operator[](int i) const { return t[i]; }
-
-    friend ostream & operator<<(ostream & os, Query query) {
-        int i;
-        for (i = 0; i < query.size() - 1; i++) {
-            os << query[i] << ", ";
-        }
-        os << query[i];
-        return os;
-    }
-
-    friend istream & operator>>(istream & is, Query &query) {
-        string s;
-        is >> s;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ',') {
-                query.t.push_back(s.substr(0, i));
-                s = s.substr(i + 1);
-                i = -1;
-            }
-        }
-        query.t.push_back(s);
-        return is;
-    }
-};
-
-class Queries {
-private:
-    vector<Query> queries;
-public:
-    Queries() = default;
-
-    explicit Queries(const vector<Query> &queries) {
-        this->queries = queries;
-    }
-    
-    ~Queries() {
-        queries.clear();
-        queries.shrink_to_fit();
-    }
-
-    [[nodiscard]] int size() { return queries.size(); }
-
-    [[nodiscard]] const Query &operator[](int i) const { return queries[i]; }
-
-    void add(const Query & q) {
-        this->queries.push_back(q);
-    }
-
-    void calc(Documents documents) {
-        for (int i = 0; i < queries.size(); ++i) {
-            queries[i].calc(documents);
-        }
-    }
-
-    double score(int index_query, int index_doc) {
-        return this->queries[index_query].get_score(index_doc);
-    }
-
-    friend ostream & operator<<(ostream & os, Queries q) {
-        int i;
-        for (i = 0; i < q.size() - 1; i++) {
-            os << q[i] << endl;
-        }
-        os << q[i];
-        return os;
-    }
-};
+    return res + 1; // return
+}
 
 int main() {
-    int n, q;
-    Documents docs;
-    Queries queries;
-
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        Document document;
-        cin >> document;
-        docs.add(document);
+    input();
+    calc_f(); // pre-calc
+    for (auto &query : query_vector) {
+        cout << search_engine(query) << endl;
     }
-
-    cin >> q;
-    for (int i = 0; i < q; ++i) {
-        Query query;
-        cin >> query;
-        queries.add(query);
-    }
-
-    queries.calc(docs);
-
-    for (int i = 0; i < queries.size(); ++i) {
-        Query q = queries[i];
-        cout << q.search_index() << endl;
-    }
-
+    
     return 0;
 }
 
