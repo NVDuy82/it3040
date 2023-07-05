d03868cceb7744dc7f74b73b70e0c194fe91967f
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-07-05 10:30:57 +0700

    Update bai5.3.cpp

--- BaiThucHanh5/bai5.3.cpp
+++ BaiThucHanh5/bai5.3.cpp
@@ -1,8 +1,18 @@
+// Nguyen Van Duy - 20215334
+/*
+Bài 5.3. Dãy ngoặc đúng
+Viết một chương trình nhận vào một dãy dấu ngoặc và kiểu tra xem dãy dấu ngoặc đóng mở đúng chưa.
+Đề bài: http://codeforces.com/group/Ir5CI6f3FD/contest/269186/problem/H
+
+Mã nguồn dưới đây là của một sinh viên, khi submit bị lỗi runtime (Exit code is -1073741819).
+Sử dụng công cụ debug ở trên, hãy tìm và sửa các lỗi trong mã nguồn
+*/
+
 #include <iostream>
 using namespace std;
 #include <string.h>
 #include <stack>
- 
+
 int par(string str){
     int a = str.length();
     stack<char> S;
@@ -13,6 +23,7 @@ int par(string str){
             S.push(x);
         }
         else {
+            if(S.size() == 0) return 0;
             if (x == ')') {
                 if (S.top() == '('){
                     S.pop();
@@ -33,21 +44,22 @@ int par(string str){
             }
         }
     }
-    if (S.top() != NULL){
+    if (S.size() != 0){
         return 0;
     }
     else return 1;
 }
- 
+
 int main(){
     int n;
     string str;
-    
+
     cin >> n;
     for(int i=0; i<n; i++){
         cin >> str;
         cout << par(str) << endl;
     }
-    
+
     return 0;
 }
+// Nguyen Van Duy - 20215334
